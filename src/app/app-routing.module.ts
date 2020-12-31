e2f828be70f51f8dc5468a0d6d39ace8265e443b
CookieRamen <ark@arkjp.net>
2020-12-31 12:42:58 +0900

    Add index page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./pages/index/index.module').then(m => m.IndexModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
