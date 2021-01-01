740e55fe0410074c58a034f1e0cd6e438b649ef4
CookieRamen <ark@arkjp.net>
2021-01-01 12:45:39 +0900

    Update Firebase appId and add comment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: true,
+  // FirebaseのWebAPIキーは公開しても大丈夫
   firebase: {
     apiKey: 'AIzaSyD3YdhoJ5sXD8de5Api2LKk7tjMqBu2gUg',
     authDomain: 'misskeyassetstore.firebaseapp.com',
     projectId: 'misskeyassetstore',
     storageBucket: 'misskeyassetstore.appspot.com',
     messagingSenderId: '855951205751',
-    appId: '1:855951205751:web:32ee827ee902258f9e2d72',
-    measurementId: 'G-KCWEJQYX3H'
+    appId: '1:855951205751:web:961dd1622a6c3f749e2d72',
+    measurementId: 'G-X6QZ36T9DN'
   }
 };
