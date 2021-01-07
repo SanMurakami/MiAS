64c912dff68126214c5b3784fe15509b0a4f7738
CookieRamen <ark@arkjp.net>
2021-01-07 20:44:58 +0900

    Add user profile page

--- src/app/pages/user/profile/profile-routing.module.ts
+++ src/app/pages/user/profile/profile-routing.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: ':id',
-    loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule)
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class UserRoutingModule { }
+export class ProfileRoutingModule { }
