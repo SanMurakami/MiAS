d7e8db81ead70f551b8884b14959aa24faea6dc5
CookieRamen <ark@arkjp.net>
2021-01-11 21:40:26 +0900

    Add user settings routing

--- src/app/pages/user/settings/settings-routing.module.ts
+++ src/app/pages/user/settings/settings-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SettingsComponent} from './settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: SettingsComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
