5452037ca1524d0ff91ac845d3895369612feb57
CookieRamen <ark@arkjp.net>
2021-01-12 21:41:42 +0900

    Add not register redirect

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,14 @@
 import {Injectable} from '@angular/core';
 import {AngularFireAuth} from '@angular/fire/auth';
 import firebase from 'firebase';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {Router} from '@angular/router';
 import auth = firebase.auth;
+
+export interface User {
+  uid: string;
+  name: string;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +16,23 @@ import auth = firebase.auth;
 export class AuthService {
   user$ = this.afAuth.user;
 
-  constructor(private afAuth: AngularFireAuth) {
+  constructor(
+    private afAuth: AngularFireAuth,
+    private afs: AngularFirestore,
+    private router: Router
+  ) {
   }
 
   login(): void {
-    this.afAuth.signInWithPopup(new auth.GoogleAuthProvider())
+    this.afAuth.signInWithPopup(new auth.GoogleAuthProvider()).then(data => {
+      this.afs.collection<User>('users', ref => ref.where('uid', '==', data.user?.uid))
+        .valueChanges()
+        .subscribe(result => {
+          if (result.length !== 1) {
+            this.router.navigate(['/user/register']);
+          }
+        });
+    });
   }
 
   logout(): void {
