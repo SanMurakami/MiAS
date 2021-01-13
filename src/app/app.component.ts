b7fdec728f215defea05c0f9e34a035699f10eb7
CookieRamen <ark@arkjp.net>
2021-01-13 22:30:14 +0900

    Automatically return to the top page after logging out.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from './services/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   user$ = this.auth.user$;
 
-  constructor(private auth: AuthService) {
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.auth.user$.subscribe(data => {
+      if (!data) {
+        this.router.navigate(['/']);
+      }
+    });
   }
 
   login(): void {
