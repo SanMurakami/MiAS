28cbd60e01b14d2b3a4cc4526d26f470c9498541
CookieRamen <ark@arkjp.net>
2021-02-01 01:54:33 +0900

    Remove default test.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,11 @@ describe('AppComponent', () => {
   it(`should have as title 'mias'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mias');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('mias app is running!');
   });
 });
