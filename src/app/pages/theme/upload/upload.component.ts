5c30e59b6139b48be3a7d8506709cfbe99ff73e3
CookieRamen <ark@arkjp.net>
2021-02-03 01:57:06 +0900

    Add theme upload page and theme validate.

--- src/app/pages/theme/upload/upload.component.ts
+++ src/app/pages/theme/upload/upload.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ThemeService} from '../../../services/theme.service';
+import {AuthService} from '../../../services/auth.service';
+import {FormControl, Validators} from '@angular/forms';
+import * as json5 from 'json5';
 
 @Component({
   selector: 'app-upload',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upload.component.scss']
 })
 export class UploadComponent implements OnInit {
+  uid: string | undefined;
+  themeValidate?: boolean;
 
-  constructor() { }
+  themeFormControl = new FormControl('', [
+    Validators.required
+  ]);
+
+  constructor(
+    private theme: ThemeService,
+    private auth: AuthService
+  ) { }
 
   ngOnInit(): void {
+    this.auth.user$.subscribe(data => {
+      this.uid = data?.uid;
+    });
+  }
+
+  upload(): void {
+    try {
+      const validate = json5.parse(this.themeFormControl.value);
+      this.themeValidate = true;
+    } catch (e) {
+      this.themeValidate = false;
+    }
   }
 
 }
--- src/app/pages/theme/upload/upload.module.ts
+++ src/app/pages/theme/upload/upload.module.ts
@@ -3,13 +3,25 @@ import { CommonModule } from '@angular/common';
 
 import { UploadRoutingModule } from './upload-routing.module';
 import { UploadComponent } from './upload.component';
-
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [UploadComponent],
   imports: [
     CommonModule,
-    UploadRoutingModule
+    UploadRoutingModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule,
   ]
 })
 export class UploadModule { }
